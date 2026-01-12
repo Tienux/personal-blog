b4c9a35125faf8816d6ee342cec637f60c78f4e4
Tienux <etienneportron@gmail.com>
2026-01-12 15:12:35 +0100

    warking navbar

--- app/components/navbar.vue
+++ app/components/navbar.vue
@@ -13,9 +13,8 @@
   
 .navbar {
   background-color: #292929;
-    padding: 1rem 0;
-  
-  
+  padding: 1rem 0;
+  overflow: hidden;
 }
 
 .navbar-container {
@@ -34,13 +33,31 @@
   vertical-align: middle;
   align-items: center;
   font-size: large;
+  position: relative;
+  transition: transform 0.2s ease, color 0.2s ease;
 }
 
+.navbar-item::before {
+  content: '';
+  position: absolute;
+  top: -1rem;
+  left: 0;
+  right: 0;
+  width: 100%;
+  height: calc(100% + 2rem);
+  background-color: #0f0f0f;
+  opacity: 0;
+  transition: opacity 0.5s ease;
+  z-index: -1;
+}
 
 .navbar-item:hover {
   color: #ff6600;
-  background-color: #0f0f0f;
-  
+  transform: scale(1.1);
+}
+
+.navbar-item:hover::before {
+  opacity: 1;
 }
 
 
