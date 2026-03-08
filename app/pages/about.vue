08f802144659e836b27d6d26d6c80b709a86cadb
Tienux <etienneportron@gmail.com>
2026-03-08 15:21:34 +0100

    debut redaction a propos

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,12 +1,18 @@
 <template>
     <div class="about">
-        <h1>Qui suis-je ?</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A itaque neque corporis nemo saepe sequi eaque nihil esse, sapiente ad eius voluptas voluptates quod repellat molestias nobis ipsum deleniti facilis.</p>
-        <h1>Qui suis-je ?</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A itaque neque corporis nemo saepe sequi eaque nihil esse, sapiente ad eius voluptas voluptates quod repellat molestias nobis ipsum deleniti facilis.</p>
-       <h1>Qui suis-je ?</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A itaque neque corporis nemo saepe sequi eaque nihil esse, sapiente ad eius voluptas voluptates quod repellat molestias nobis ipsum deleniti facilis.</p>
-  
+        <h2>Qui suis je ? </h2>
+        <p>Je m'appelle Etienne, j'ai 23 ans, et je suis un passioné d'informatique</p>
+        <h2>Mon parcours </h2>
+        <p>Diplomé à l'université de Poitiers. J'y ai obtenu ma licence en informatique en 2024 
+            avant de partir à Tours pour le nouveau Master Intelligent System and Application (ISA). 
+            Ce dernier est actuellement en cours d'obtention.
+        </p>
+        <h2>Pourquoi ce blog ?</h2>
+        <p>J'utilise ce blog pour publier mes activités externes en lien avec la tech. 
+            Vous pourrez retrouver mes collaborations sur Youtube,
+             mes explorations techniques ainsi que mes différents projets externes.
+            Le contenu de ce site est publié sous la licence <a href="https://creativecommons.org/licenses/by/4.0/">Creative Commons CC BY license</a>
+        </p>
     </div>
 </template>
 
